ee511f0e50bdbdca3a4800f174cef7a4d8c2e2cb
i-metlyuk <igor.m@core-tech.ru>
2022-01-11 16:33:58 +0300

    starting react+redux

--- src/index.js
+++ src/index.js
@@ -1,63 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 
-const initialState = {
-  value: 1,
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INC': 
-      return {
-        ...state,
-        value: state.value + 1
-      };
-    case 'DEC': 
-      return {
-        ...state,
-        value: state.value - 1
-      };
-    case 'RND': 
-      return {
-        ...state,
-        value: state.value * action.payload
-      };
-    default:
-      return state
-  }
-}
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
-}
-
-store.subscribe(update);
-
-const inc = () => ({type: "INC"});
-const dec = () => ({type: "DEC"});
-const rnd = (value) => ({type: "RND", payload: value});
-
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-})
-
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-})
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 100);
-  store.dispatch(rnd(value));
-})
-
 ReactDOM.render(
   <React.StrictMode>
-    <>
-      
-    </>
+    <Provider store={store}>
+      <App></App>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
